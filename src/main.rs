e0c96d59f0a0fb4a42a14647e50dc2946c1120e2
tianen <tianen@tianendeMacBook-Pro.local>
2020-02-27 21:04:06 +0800

    find_nth_digit

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod leetcode;
 mod offer;
 
 fn main() {
-    offer::find_nth_digit::Solution::find_nth_digit(97);
+    offer::find_nth_digit::Solution::find_nth_digit(99);
     // offer::get_least_numbers::Solution::get_least_numbers(vec![0,0,1,2,4,2,2,3,1,4,8],5);
     // offer::verify_postorder::Solution::verify_postorder(vec![5,4,3,2,1]);
     // offer::validate_stack_sequences::Solution::validate_stack_sequences(vec![1,2,3,4,5], vec![5,4,3,2,1]);
---
+++ src/offer/find_nth_digit.rs
@@ -0,0 +1,39 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn find_nth_digit(n: i32) -> i32 {
+        let (mut len,mut cnt,mut start,mut n) = (1_i64,9_i64,1_i64,n as i64);
+        while n as i64 > len * cnt{
+            n -= len * cnt;
+            len +=1;
+            cnt *= 10 ;
+            start *= 10;
+        }
+        start += (n - 1) / len ;
+        let idx = len - (n-1) % len;
+        for _ in 1..idx{
+            start /=10;
+        }
+        start as i32 % 10
+    }
+}
+
+#[test]
+fn check() {
+    assert_eq!(Solution::find_nth_digit(3),3);
+    assert_eq!(Solution::find_nth_digit(99),4);
+}
+
+// 0-9              10 位     1 *9
+
+// 10-19            20 位
+
+// 10 -99           179位     20 * 9 -1
+
+// 100-199          300 位
+
+// 100-999          2700 位   300 * 9
+
+// 1000-1999        4000
+
+// 1000-9999        36000     4000 * 9
