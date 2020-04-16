25c602555b31e5711d00467f838d2f0c3b2fff08
tianen <cteeye@gmail.com>
2020-04-16 23:26:05 +0800

    Some solution

---
+++ src/leetcode/merge.rs
@@ -0,0 +1,80 @@
+struct Solution;
+
+impl Solution {
+    pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let len = intervals.len();
+        if len == 0 {
+            return vec![];
+        } else if len == 1 {
+            return intervals;
+        }
+        Self::quick_sort(0, intervals.len() - 1, &mut intervals);
+        let mut res = vec![];
+        let mut i = 0;
+        while i < len {
+            if i < len - 1 && intervals[i][1] >= intervals[i + 1][1]{
+                intervals[i + 1] = intervals[i].clone();
+            }
+            else if i < len - 1 && intervals[i][1] >= intervals[i + 1][0] && intervals[i][1] <= intervals[i + 1][1] {
+                intervals[i + 1] = vec![intervals[i][0], intervals[i + 1][1]];
+            } else {
+                res.push(intervals[i].clone());
+            }
+            i += 1;
+        }
+        res
+    }
+
+    fn quick_sort(start: usize, end: usize, intervals: &mut Vec<Vec<i32>>) {
+        if start < end {
+            let (mut i, mut j) = (start, end);
+            let base = intervals[i].clone();
+            while i < j {
+                while i < j && !Self::cmp(&base,&intervals[j]) {
+                    j -= 1;
+                }
+                intervals[i] = intervals[j].clone();
+                while i < j && Self::cmp(&base,&intervals[i]) {
+                    i += 1;
+                }
+                intervals[j] = intervals[i].clone();
+            }
+            intervals[i] = base;
+            if i > 1 {
+                Self::quick_sort(start, i - 1, intervals);
+            }
+            Self::quick_sort(i + 1, end, intervals);
+        }
+    }
+
+    //letf >= right true
+    fn cmp(l:&Vec<i32>,r:&Vec<i32>)->bool{
+        if l[0]>r[0]{
+            true
+        }
+        else if l[0]<r[0]{
+            false
+        }
+        else{
+            if l[1] >= r[1] {
+                true
+            }
+            else{
+                false
+            }
+        }
+    }
+}
+
+#[test]
+fn check() {
+    let mut v = vec![vec![0, 1], vec![2, 3], vec![1, 2]];
+    Solution::quick_sort(0, v.len() - 1, &mut v);
+    assert_eq!(v, vec![vec![0, 1], vec![1, 2], vec![2, 3]]);
+    let mut v = vec![vec![0, 1]];
+    Solution::quick_sort(0, v.len() - 1, &mut v);
+    assert_eq!(v, vec![vec![0, 1]]);
+
+    assert_eq!(Solution::merge(vec![vec![1,4],vec![1,5]]),vec![vec![1,5]]);
+    assert_eq!(Solution::merge(vec![vec![1,4],vec![2,3]]),vec![vec![1,4]]);
+}
--- src/leetcode/update_matrix.rs
+++ src/leetcode/update_matrix.rs
@@ -3,14 +3,14 @@ struct Solution;
 impl Solution {
     pub fn update_matrix(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         let len = matrix.len();
-        let mut res = vec![vec![0;len]];
+        let mut res = vec![vec![0; len]];
         let mut visited;
         //遍历每一个点
-        for x in 0..len{
-            for y in 0..len{
-                if (matrix[y][x] == 1){
+        for x in 0..len {
+            for y in 0..len {
+                if (matrix[y][x] == 1) {
                     //初始化访问节点
-                    visited = vec![vec![false];len];
+                    visited = vec![vec![false]; len];
                     //give up
                 }
             }
@@ -19,16 +19,13 @@ impl Solution {
         vec![]
     }
 
-
-    fn bfs(x:usize,y:usize,len:usize,visited:Vec<Vec<bool>>,matrix:Vec<Vec<i32>>){
+    fn bfs(x: usize, y: usize, len: usize, visited: Vec<Vec<bool>>, matrix: Vec<Vec<i32>>) {
         //边界内且未被访问过
-        if x > 0 && x < len && y > 0 && y < len{
-            if matrix[y][x] == 1{//入口节点
-
+        if x > 0 && x < len && y > 0 && y < len {
+            if matrix[y][x] == 1 { //入口节点
             }
             //遍历途中
-            else if !visited[y][x]{
-
+            else if !visited[y][x] {
             }
         }
     }
